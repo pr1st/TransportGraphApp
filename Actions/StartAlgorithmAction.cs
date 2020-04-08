1f178866e284d0a738470e1170355095fdba50ba
Dmitry Dymov <dymovst@gmail.com>
2020-04-08 20:28:34 +0700

    preperad algorithm config window for amnipulations

--- Actions/StartAlgorithmAction.cs
+++ Actions/StartAlgorithmAction.cs
@@ -1,7 +1,9 @@
-﻿namespace TransportGraphApp.Actions {
+﻿using TransportGraphApp.Dialogs;
+
+namespace TransportGraphApp.Actions {
     public static class StartAlgorithmAction {
         public static void Invoke() {
-            
+            new StartAlgorithmDialog().ShowDialog();
         }
     }
 }
